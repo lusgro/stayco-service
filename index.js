40820d204c78ff01267b1c794c58ab05d36374ee
Your Name <lucasurielrosenbaum@gmail.com>
2024-04-14 23:29:12 -0300

    manejo de errores mediante middleware

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getAccessToken } = require('./getAcessToken');
-const { connectSql, disconnectSql, getArtists } = require('./bd/bd');
+const { connectSql, } = require('./bd/bd');
 const { getNewArtist } = require('./getNewArtist');
 const { saveArtist } = require('./bd/saveArtist');
 const { getAlbums } = require('./getAlbums');
@@ -19,33 +19,40 @@ async function main() {
     const token = await getAccessToken();
     console.log(token);
 
-    app.get('/obtenerartista', async (req, res) => {
-        const artistName = req.query.nombre;
-        if (artistName == null) {
-            res.status(400).send('No se ingreso un nombre de artista');
-            return;
+    app.use((err, req, res, next) => {
+        console.error(err);
+        res.status(500).send(`Error: ${err.message}`);
+    });
+
+    app.get('/obtenerartista', async (req, res, next) => {
+        const { nombre: artistName } = req.query;
+        if (!artistName) {
+            return next(new Error('No se ingreso un nombre de artista'));
         }
         await connectSql();
         try {
             await saveNewArtist(artistName, token);
             res.status(200).send('Artista guardado correctamente');
         } catch (err) {
-            console.error(err);
-            res.status(500).send('Error al guardar el artista');
+            next(err);
         }
     });
 }
 
 async function saveNewArtist(artist, token) {
     const newArtist = await getNewArtist(artist, token);
-    const artist_id_bd = await saveArtist(newArtist);
+    if (newArtist) {
+        const artist_id_bd = await saveArtist(newArtist);
 
-    const albums = await getAlbums(newArtist.ID_Spotify, artist_id_bd, token);
-    await Promise.all(albums.map(async (album) => {
-        const album_id_bd = await saveAlbum(album);
-        const songs = await getSongs(album.ID_Spotify, album_id_bd, token);
-        await Promise.all(songs.map((song) => saveSong(song, album_id_bd)));
-    }));
+        const albums = await getAlbums(newArtist.ID_Spotify, artist_id_bd, token);
+        await Promise.all(albums.map(async (album) => {
+            const album_id_bd = await saveAlbum(album);
+            const songs = await getSongs(album.ID_Spotify, album_id_bd, token);
+            await Promise.all(songs.map((song) => saveSong(song, album_id_bd)));
+        }));
+    } else {
+        console.log("Artista nulo")
+    }
 }
 
 main();
